58980822cd1c635f8c066017edd74f4760e16880
FadhilAmad <fadhilsyawaludin5@gmail.com>
2026-04-26 21:09:43 +0700

    set up API

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,20 @@
 from flask import Flask
-from .config import db_connection
+from .config import  db_connection
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
 
     #untuk memanggil config
     app.config.from_object(config)
