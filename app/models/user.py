27260df8aabaa21e09b5b0e74026fac15e02e0b5
FadhilAmad <fadhilsyawaludin5@gmail.com>
2026-04-27 21:57:46 +0700

    pembuatan models untuk tabel di data bse

--- app/models/user.py
+++ app/models/user.py
@@ -1,1 +1,21 @@
- 
+import uuid
+from app import db, bcrypt
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    propfile_img = db.Column(db.String(255))
+    thumbnail_img = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self, self.password_hash, password)     
