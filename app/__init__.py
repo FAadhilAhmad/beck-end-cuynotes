d6172a053deadf4abef5d53d889b767def616b8c
FadhilAmad <fadhilsyawaludin5@gmail.com>
2026-05-16 20:53:08 +0700

    created table users

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from .config import  db_connection
+from .config import Config, db_connection
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-
+#untuk inisialisasi 
 bcrypt = Bcrypt()
 db = SQLAlchemy()
 migrate = Migrate()
@@ -12,15 +12,21 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+
+    #untuk memanggil config
+    app.config.from_object(Config)
+    #untuk tes koneksi database
+    db_connection()
+
+    # untuk inisialisasi database, migrasi, dan bcrypt
     db.init_app(app)
     migrate.init_app(app, db)
     bcrypt.init_app(app)
 
-    #untuk memanggil config
-    app.config.from_object(config)
-    #untuk tes koneksi database
-    db_connection()
+    #daftar models
+    from app.models import user
+
     #untuk memanggil route
-    from .routes.base_routes import base
+    from app.routes.base_routes import base
     app.register_blueprint(base, url_prefix="/")
     return app
--- app/models/user.py
+++ app/models/user.py
@@ -3,19 +3,21 @@ from app import db, bcrypt
 from datetime import datetime
 
 class User(db.Model):
+    #nama table
     __tablename__ = "users"
-
-    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
-    propfile_img = db.Column(db.String(255))
-    thumbnail_img = db.Column(db.String(255))
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(50), nullable=False)
+    profile_img = db.Column(db.String(255))
+    thumbnail_img = db.Column(db.String(255))
+    created_ad = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_ad = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
     def set_password(self, password):
-        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+        self.password_hash= bcrypt.generate_password_hash(password).decode("utf-8")
 
     def check_password(self, password):
-        return bcrypt.check_password_hash(self, self.password_hash, password)     
+        return bcrypt.check_password_hash(self.password_hash, password)    
